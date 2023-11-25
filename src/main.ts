7f9ba794e0e3ab79215ed3832b0aa86305db9f76
baizhi958216 <1475289190@qq.com>
2023-11-25 13:43:29 +0800

    feat: update styles and env
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./routes/router";
-
+import "virtual:uno.css";
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div class="p-6 flex flex-col gap-2">
     <div>买点什么好呢(・∀・(・∀・(・∀・*)？</div>
-    <br>
-    <div>名字: <br/><input type="text" v-model="productName" /></div>
-    <div>价格: <br/>￥<input type="number" v-model="productValue" /></div>
-    <br>
-    <button @click="pay">付款</button>
+    <div class="flex gap-3 items-center">
+      <div><input class="outline-none p-3 rounded" placeholder="商品名" type="text" v-model="productName" />
+      </div>
+      <div>￥<input class="outline-none p-3 rounded" placeholder="商品价格" type="number" v-model="productValue" />
+      </div>
+    </div>
+    <button
+      class="w-[120px] bg-blue-400 outline-none border-none rounded p-2 cursor-pointer text-white hover:bg-blue-300 transition"
+      @click="pay">付款</button>
   </div>
 </template>
 
@@ -15,10 +19,13 @@ import { ref } from "vue";
 const productName = ref("");
 const productValue = ref("");
 const pay = () => {
+  if (!productName.value && !productValue.value) {
+    return
+  }
   fetch(
-    `http://localhost:3000/alipay/pay?product_name=${productName.value}&product_value=${productValue.value}`
+    `${import.meta.env.VITE_BACKEND_URL}/alipay/pay?product_name=${productName.value}&product_value=${productValue.value}`
   ).then((res) => {
-    window.open(res.url)
+    window.open(res.url, "_self")
   });
 };
 </script>
